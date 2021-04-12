aac346752b73b22474885cbedec3e1ac8a5adfc9
Rayson-LD <72185590+Rayson-LD@users.noreply.github.com>
2021-04-12 13:09:43 +0530

    Update readme.md

--- front-end/Aboutus.blade.php
+++
@@ -1,5 +0,0 @@
-@include('front-end.layouts.head')
-
-<h1>This is About Us</h1>
-
-@include('front-end.layouts.footer')
--- front-end/report.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>
-</title>
-</head>
-<body>
-<img src="images/report.gif" style="width:100%;height:100%;" />
-</body>
-</html>
